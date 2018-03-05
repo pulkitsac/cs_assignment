21155674735346997fa6db0693311a2211be1c00
pulkitsac <pulkitsac@github.com>
2018-03-05 06:08:22 +0530

    Step 19

--- hello.c
+++ hello.c
@@ -2,6 +2,8 @@
 
 void microkernel_sendmsg(char *);
 
+void microkernel_getmsg(char *);
+
 void main(){
 	printf("Helloworld!\n");
 	printf("This must be a monolithic design\n");
@@ -10,5 +12,9 @@ void main(){
 
 void microkernel_sendmsg(char *a){
 	printf("microkernel: %s\n", a);
+}
+
+void microkernel_getmsg(char *b){
+	//TODO:getmsg feature
 }
 
